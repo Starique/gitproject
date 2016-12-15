e7dbaff80e1f84376e4cbd2b6413e6c507fa4d0a
Tarique Syed <syed.tarique@gmail.com>
2016-12-15 14:42:19 -0500

    Added webskeliton first time

---
+++ webskeliton/src/main/java/com/webskeliton/WebskelitonApplication.java
@@ -0,0 +1,12 @@
+package com.webskeliton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebskelitonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebskelitonApplication.class, args);
+	}
+}
