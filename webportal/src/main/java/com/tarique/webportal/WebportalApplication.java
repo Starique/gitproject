02548a2671a9eb59e729fb1e1a9af563cc4b412a
Tarique Syed <syed.tarique@gmail.com>
2016-12-16 12:22:26 -0500

    "Adding Web Portal"

---
+++ webportal/src/main/java/com/tarique/webportal/WebportalApplication.java
@@ -0,0 +1,12 @@
+package com.tarique.webportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebportalApplication.class, args);
+	}
+}
