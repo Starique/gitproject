9221b3c0535b8be4d20546d2542787814f53cabe
Tarique Syed <syed.tarique@gmail.com>
2016-12-22 22:44:45 -0500

    Added Bootstrap Plugin and Index Page

--- webportal/src/main/java/com/tarique/webportal/com/tarique/webportal/controller/IndexController.java
+++ webportal/src/main/java/com/tarique/webportal/com/tarique/webportal/controller/IndexController.java
@@ -4,13 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * Created by Mehnuma on 12/16/2016.
+ * Created by Mehnuma on 12/22/2016.
  */
-
-//@Controller
-public class HelloWorldController {
-    ///@RequestMapping("/")
-    //public String sayHello() {
-        //return "index";
-   // }
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public String home() {
+        return "index";
+    }
 }
