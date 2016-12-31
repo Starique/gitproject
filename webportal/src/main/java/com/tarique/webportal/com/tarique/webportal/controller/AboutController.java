afb9da6973df9827146c57d32f515eace323f4a1
Tarique Syed <syed.tarique@gmail.com>
2016-12-31 14:49:48 -0500

    Added the About Us Page

---
+++ webportal/src/main/java/com/tarique/webportal/com/tarique/webportal/controller/AboutController.java
@@ -0,0 +1,15 @@
+package com.tarique.webportal.com.tarique.webportal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Mehnuma on 12/28/2016.
+ */
+@Controller
+public class AboutController {
+    @RequestMapping("/about")
+    public String about() {
+        return "copy/about";
+    }
+}
---
+++ webportal/src/main/resources/static/js/webportal.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Mehnuma on 12/31/2016.
+ */
+$( document ).ready( main );
+
+function main() {
+
+    $('.btn-collapse').click(function(e) {
+        e.preventDefault();
+        var $this = $(this);
+        var $collapse = $this.closest('.collapse-group').find('.collapse');
+        $collapse.collapse('toggle');
+    });
+
+}
