432db5fb92b1a3f67d465936f24f55b52896c627
Tarique Syed <syed.tarique@gmail.com>
2017-01-12 07:15:21 -0500

    Added all JPA classes for spring security

---
+++ webportal/src/main/java/com/tarique/webportal/com/tarique/webportal/backend/persistence/domain/backend/Role.java
@@ -0,0 +1,51 @@
+package com.tarique.webportal.com.tarique.webportal.backend.persistence.domain.backend;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Mehnuma on 1/12/2017.
+ */
+@Entity
+public class Role implements Serializable{
+    /** The serial Version UID for Serialazable classes. */
+    private static final long serialVersionUID = 1L;
+
+
+    @Id
+    private int id;
+
+    private String name;
+
+    public Role() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @OneToMany(mappedBy = "role", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private Set<UserRole> userRoles = new HashSet<>();
+
+    public Set<UserRole> getUserRoles() {
+        return userRoles;
+    }
+
+    public void setUserRoles(Set<UserRole> userRoles) {
+        this.userRoles = userRoles;
+    }
+}
